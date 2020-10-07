ace401b2ac0a55b79ff2434b723f13358b1cd876
Eric Wong <ericwong12@live.com>
2020-10-07 12:12:16 -0600

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="">
+    <h1>Statements</h1>
+    <pre>{{ statements }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      statements: [],
+    }
+  },
+  async fetch() {
+    this.statements = await this.$content('statements').fetch()
+  },
+}
+</script>
+
+<style>
+</style>
