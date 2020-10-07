4aed654901c44a75a205ec1eb1a9ec6722c7cf95
Eric Wong <ericwong12@live.com>
2020-10-07 16:04:42 -0600

    Updated styling and functionality Will need to export home data to components

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
+Vue.use(Buefy)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: function (h) { return h(App) }
+  render: function (h) {
+    return h(App)
+  }
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <ul class="datas" v-for="transaction in data" :key="transaction.id">
-      <li>{{ transaction.Category }}</li>
-      <li>{{ transaction.Description }}</li>
-      <li>{{ transaction["Transaction Date"] }}</li>
-      <li>{{ transaction.Type }}</li>
-      <li>{{ transaction.Amount }}</li>
-    </ul>
-
-    <h1>{{ expenses }}</h1>
-    <h2>{{ totalExpenses }}</h2>
+  <div class="container">
+    <table class="tableData">
+      <tr>
+        <th>Category</th>
+        <th>Description</th>
+        <th>Transaction Date</th>
+        <th>Type</th>
+        <th>Amount</th>
+      </tr>
+      <tr v-for="transaction in data" :key="transaction.id">
+        <td>{{ transaction.Category }}</td>
+        <td>{{ transaction.Description }}</td>
+        <td>{{ transaction["Transaction Date"] }}</td>
+        <td>{{ transaction.Type }}</td>
+        <td>{{ transaction.Amount }}</td>
+      </tr>
+    </table>
+    <div class="expenses">
+      <!-- <div class="">
+        <h1>Expenses:</h1>
+        <p>{{ expenses }}</p>
+      </div> -->
+      <div class="totaledExpenses">
+        <h1>Total Expenses: $ {{ totalExpenses }}</h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import * as d3 from "d3";
 export default {
   name: "Home",
@@ -26,18 +39,22 @@ export default {
       totalExpenses: null,
     };
   },
+  //LifeCycle hooks. Called asynchronous allow data to populate and be used.
   async created() {
     await this.loadData();
     await this.expense();
   },
 
   methods: {
+    //loads information from csv file and returns it to data
     async loadData() {
       await d3.csv("/statements/chase.csv").then((data) => {
         console.log(data);
         this.data = data;
       });
     },
+    //Takes data and for each amount push it to expenses.
+    //Once pushed take that data from expenses array and combine them
     async expense() {
       await this.data.forEach((el) => {
         console.log(el.Amount);
@@ -55,3 +72,14 @@ export default {
   },
 };
 </script>
+<style scoped>
+.container {
+  display: flex;
+}
+.tableData {
+  flex: 0 0 70%;
+}
+.expenses {
+  flex: 0 0 30%;
+}
+</style>
